2305e460f8d1f869b757130671eb928aac85c811
GnahkaiG <91893724+GnahkaiG@users.noreply.github.com>
2024-03-15 10:07:51 +0700

    Revert về commit 2

--- Baitap/Form1.Designer.cs
+++ Baitap/Form1.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.UserName = new System.Windows.Forms.Label();
             this.txtUserName = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.Label();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // UserName
@@ -48,15 +51,46 @@
             this.txtUserName.Size = new System.Drawing.Size(100, 20);
             this.txtUserName.TabIndex = 1;
             // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.Location = new System.Drawing.Point(113, 99);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(53, 13);
+            this.Password.TabIndex = 2;
+            this.Password.Text = "Password";
+            this.Password.Click += new System.EventHandler(this.Password_Click);
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(201, 97);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(100, 20);
+            this.txtpassword.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(116, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.Password);
             this.Controls.Add(this.txtUserName);
             this.Controls.Add(this.UserName);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -66,6 +100,9 @@
 
         private System.Windows.Forms.Label UserName;
         private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label Password;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Baitap/Form1.cs
+++ Baitap/Form1.cs
@@ -16,5 +16,33 @@ namespace Baitap
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Password_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = txtUserName.Text;
+            string password = txtpassword.Text;
+
+       
+            {
+                // Đăng nhập thành công
+                MessageBox.Show("Đăng nhập thành công!");
+                // Thực hiện các hành động khác sau khi đăng nhập thành công (nếu cần)
+                Form2 successForm = new Form2();
+                this.Hide();
+                successForm.ShowDialog();
+                this.Close();
+            }
+          
+        }
     }
 }
