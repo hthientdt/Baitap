60a0cfbc018a375bf10258dd4868a71de052fe59
GnahkaiG <91893724+GnahkaiG@users.noreply.github.com>
2024-03-15 09:23:03 +0700

    Label UserName, textbox UserName

--- Baitap/Form1.Designer.cs
+++ Baitap/Form1.Designer.cs
@@ -28,13 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.UserName = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // UserName
+            // 
+            this.UserName.AutoSize = true;
+            this.UserName.Location = new System.Drawing.Point(113, 68);
+            this.UserName.Name = "UserName";
+            this.UserName.Size = new System.Drawing.Size(57, 13);
+            this.UserName.TabIndex = 0;
+            this.UserName.Text = "UserName";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(201, 65);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(100, 20);
+            this.txtUserName.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.UserName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label UserName;
+        private System.Windows.Forms.TextBox txtUserName;
     }
 }
 
