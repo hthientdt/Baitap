440167889570c2bdfa523c94dd4b258d163b08cb
truong thien <h.thienthd@gmail.com>
2024-03-15 09:38:35 +0700

    Label Mật Khẩu, textbox Mật Khẩu

--- Baitap/Form1.Designer.cs
+++ Baitap/Form1.Designer.cs
@@ -30,33 +30,59 @@
         {
             this.UserName = new System.Windows.Forms.Label();
             this.txtUserName = new System.Windows.Forms.TextBox();
+            this.MatKhau = new System.Windows.Forms.Label();
+            this.txtmatkhau = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // UserName
             // 
             this.UserName.AutoSize = true;
-            this.UserName.Location = new System.Drawing.Point(113, 68);
+            this.UserName.Location = new System.Drawing.Point(151, 84);
+            this.UserName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.UserName.Name = "UserName";
-            this.UserName.Size = new System.Drawing.Size(57, 13);
+            this.UserName.Size = new System.Drawing.Size(73, 16);
             this.UserName.TabIndex = 0;
             this.UserName.Text = "UserName";
             // 
             // txtUserName
             // 
-            this.txtUserName.Location = new System.Drawing.Point(201, 65);
+            this.txtUserName.Location = new System.Drawing.Point(268, 80);
+            this.txtUserName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(100, 20);
+            this.txtUserName.Size = new System.Drawing.Size(132, 22);
             this.txtUserName.TabIndex = 1;
+            // 
+            // MatKhau
+            // 
+            this.MatKhau.AutoSize = true;
+            this.MatKhau.Location = new System.Drawing.Point(151, 122);
+            this.MatKhau.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.MatKhau.Name = "MatKhau";
+            this.MatKhau.Size = new System.Drawing.Size(62, 16);
+            this.MatKhau.TabIndex = 2;
+            this.MatKhau.Text = "Mật Khẩu";
+            // 
+            // txtmatkhau
+            // 
+            this.txtmatkhau.Location = new System.Drawing.Point(268, 119);
+            this.txtmatkhau.Margin = new System.Windows.Forms.Padding(4);
+            this.txtmatkhau.Name = "txtmatkhau";
+            this.txtmatkhau.Size = new System.Drawing.Size(132, 22);
+            this.txtmatkhau.TabIndex = 3;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.txtmatkhau);
+            this.Controls.Add(this.MatKhau);
             this.Controls.Add(this.txtUserName);
             this.Controls.Add(this.UserName);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -66,6 +92,8 @@
 
         private System.Windows.Forms.Label UserName;
         private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label MatKhau;
+        private System.Windows.Forms.TextBox txtmatkhau;
     }
 }
 
